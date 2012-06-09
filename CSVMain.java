89d06300b63f506358a2ff2efa0a47f0ed5edbc5
theNerd247 <harvey.coder247@gmail.com>
2012-06-08 23:07:43 -0400

    update

---
+++ CSVMain.java
@@ -0,0 +1,20 @@
+import java.util.Vector;
+public class CSVMain 
+{
+	public static void main(String[] args)
+	{
+		CSVFile file = new CSVFile("test.txt");//"C:\\Users\\Developer\\Desktop\\GIT\\JavaCSVReader\\test.txt");
+
+		Vector dta = file.getData();
+		CSVDataHeader header = (CSVDataHeader)dta.elementAt(0);
+		Vector data = header.getData();
+		Vector intData = (Vector)(data.elementAt(0));
+		//Integer integer= (Integer)(intData.elementAt(1));
+		String[] name = header.getHeader();
+		for(String i : name)
+			System.out.print(i+",");
+		
+		System.out.println("\nend");
+		
+	}
+}	
