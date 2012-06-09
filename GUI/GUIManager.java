3b317c99afc846ac21763310a8868ef488006413
theNerd247 <harvey.coder247@gmail.com>
2012-06-09 14:55:36 -0400

    resetting

---
+++ GUI/GUIManager.java
@@ -0,0 +1,26 @@
+//class that implements program managing and control
+public class GUIManager
+{
+	public static void open()
+	{}
+	public static void close()
+	{}
+	public static void exit()
+	{}
+	public static void newFile()
+	{}
+	public static void save()
+	{}
+	public static void saveCopy()
+	{}
+	public static void saveAs()
+	{}
+	public static void newRobotSheet()
+	{}
+//	public static DataSheet getCurrentFile()
+//	{}
+	public static void showHelp()
+	{}
+	public static void showAbout()
+	{}
+}
